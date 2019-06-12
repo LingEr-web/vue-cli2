5af3570d134af38478b04c1a0f72f1ce618ed117
15093163843@163.com <15093163843@163.com>
2019-06-12 23:15:53 +0800

    筛选

--- src/views/elementUI/data/table/index.vue
+++ src/views/elementUI/data/table/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h4>排序(sortable为基数排序 :default-sort = "{prop: 'name', order: 'descending'}"默认降序)</h4>
+    <h4>筛选(filters数组写筛选的多选内容，filter-method写筛选的逻辑直接返回true和false就可以控制显示隐藏)</h4>
+    <h4>formatter(格式化数据，可以动态修改数据)</h4>
     <el-table
       :data="tableData"
       style="width: 100%"
@@ -13,7 +15,10 @@
       <el-table-column
         prop="name"
         label="姓名"
-        sortable>
+        sortable
+        :filters="[{text: 'a', value: 'a'}, {text: 'b', value: 'b'}, {text: 'c', value: 'c'}, {text: 'd', value: 'd'}]"
+        :filter-method="filterName"
+      >
       </el-table-column>
       <el-table-column
         prop="address"
@@ -51,6 +56,10 @@
       methods: {
         formatter(row, column) {
           return row.address+"123456789";
+        },
+        filterName(value, row,column){
+          const property = column['property'];
+          return row[property] === value;
         }
       }
     }
