8ec576f6b0c86280b1e21e80b6f5dafe680f8b17
15093163843@163.com <15093163843@163.com>
2019-06-12 22:53:16 +0800

    表格排序

--- src/views/elementUI/data/index.vue
+++ src/views/elementUI/data/index.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-      <badge></badge>
-      <pagination></pagination>
-      <progressed></progressed>
-      <tabled></tabled>
-      <tag></tag>
-      <tree></tree>
-    </div>
+  <div class="app-container">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="badge" name="badge"><badge></badge></el-tab-pane>
+      <el-tab-pane label="pagination" name="pagination"><pagination></pagination></el-tab-pane>
+      <el-tab-pane label="progressed" name="progressed"><progressed></progressed></el-tab-pane>
+      <el-tab-pane label="tabled" name="tabled"><tabled></tabled></el-tab-pane>
+      <el-tab-pane label="tag" name="tag"><tag></tag></el-tab-pane>
+      <el-tab-pane label="tree" name="tree"><tree></tree></el-tab-pane>
+    </el-tabs>
+  </div>
+
 </template>
 
 <script>
@@ -25,6 +28,16 @@
           tabled,
           tag,
           tree
+        },
+        data() {
+          return {
+            activeName: 'badge'
+          };
+        },
+        methods: {
+          handleClick(tab, event) {
+            console.log(tab, event);
+          }
         }
     }
 </script>
--- src/views/elementUI/data/table/index.vue
+++ src/views/elementUI/data/table/index.vue
@@ -1,10 +1,58 @@
 <template>
-<div>table</div>
+  <div>
+    <h4>排序(sortable为基数排序 :default-sort = "{prop: 'name', order: 'descending'}"默认降序)</h4>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      :default-sort = "{prop: 'name', order: 'descending'}"
+    >
+      <el-table-column
+        prop="date"
+        label="日期">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        sortable>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+        :formatter="formatter">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "index"
+      name: "index",
+      data() {
+        return {
+          tableData: [{
+            date: '2016-05-02',
+            name: 'a',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: 'b',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: 'd',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: 'c',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+        }
+      },
+      methods: {
+        formatter(row, column) {
+          return row.address+"123456789";
+        }
+      }
     }
 </script>
 
